9dc77ca28f0d8d6fa0d26be2ad874b2b31d1e5b2
EvandroCunha <sr.evanndro@gmail.com>
2020-11-13 21:27:01 -0300

    continuação

--- db.js
+++ db.js
@@ -14,7 +14,22 @@ connect();
 
 async function selectCustomers(){
     const conn = await connect();
-    return await conn.query('SELECT * FROM cliente;');
+    const [rows] = conn.query('SELECT * FROM cliente');
+    return await rows;
 }
 
-module.exports = { selectCustomers}
+async function insertCustomers(customer){
+    const conn = await connetc();
+    const sql = 'INSERT INTO cliente(nome, telefone, data-nascimento, endereço, bairro, cep, ponto-referencia, email) VALUES (?,?,?);';
+    const values = [customer.nome, customer.telefone, customer.data-nascimento, customer.endereço, customer.bairro, customer.cep, customer.ponto-referencia, customer.email];
+    await conn.query(sql, values);
+}
+
+async function updateCustomer(id,customer){
+   const conn = await connect();
+   const sql = 'UPDATE cliente SET nome=?, telefone=?, data-nascimento=?, endereço=?, bairro=?, cep=?, ponto-referencia, email=?';
+   const values = [customer.nome, customer.telefone, customer.data-nascimento, customer.endereço, customer.bairro, customer.cep, customer.ponto-referencia, customer.email];
+   await conn.query(sql, values);
+}
+
+module.exports = { selectCustomers, insertCustomers}
--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 (async () => {
     const db = require("./db");
     console.log('Começou!');
-    console.log('SELECT * FROM cliente');
+    console.log('INSERT INTO cliente');
+    await db.insertCustomers({nome:"João", telefone: 28999659841, data_nascimento: 16/01/1952, endereço: "RuaEdmundodosSantos",
+    bairro: "caixa_Z", cep:29304150, ponto_referencia: "RuaEdmundodosSantos", email:"joão22@gmail.com"});
+    console.log('SELECT * FROM cliente');   
     const cliente = await db.selectCustomers();
     console.log(cliente);
 })();
